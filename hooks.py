a1083559afdc4099edb2ca0c3f627fe72cdb21cf
Kindiy31 <71168657+Kindiy31@users.noreply.github.com>
2023-08-14 18:53:17 +0300

    a

---
+++ hooks.py
@@ -0,0 +1,29 @@
+from flask import jsonify
+import flask
+import os
+import sys
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+
+sys.path.insert(0, parent_dir)
+import os
+app = flask.Flask(__name__, static_url_path='/static')
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/hook/', methods=['GET', 'POST'], strict_slashes=False)
+def webhook_handler():
+    response_text = ('POST-запит оброблено успішно')
+    os.system('git pull')
+    os.system('systemctl restart masters')
+    print(response_text)
+    return jsonify({'message': response_text}), 200
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,
+            host='0.0.0.0',
+            port=5000)
